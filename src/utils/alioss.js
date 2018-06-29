09d7b686e3549cf7dd606b26f3dbcdb473aeb310
简小咖 <188476399@qq.com>
2018-06-29 18:29:06 +0800

    Update alioss.js

--- src/utils/alioss.js
+++ src/utils/alioss.js
@@ -1,10 +1,10 @@
 var OSS = require('ali-oss').Wrapper
 export function client(data) {
   var client = new OSS({
-    region: 'oss-cn-beijing',
-    accessKeyId: 'LTAIKWWEWhqqYURR',
-    accessKeySecret: 'iuhKgozK0NO28QGGxcUEA2yhRu7VVz',
-    bucket: 'fdfde4ba61ba4e4ea411a7d81a5894df'
+    region: '',
+    accessKeyId: '',
+    accessKeySecret: '',
+    bucket: ''
   })
   return client
 }
